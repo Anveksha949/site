4fef3a4833af9164310b7a31d2d2844074b06d68
Anveksha949 <84616158+Anveksha949@users.noreply.github.com>
2022-02-21 17:54:40 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@data new.route('/'
+def home():
+    return render_template('index 1.php')
+
+if_name_ == "_main_":
+data new.run(debug=True)
---
+++ index 1.php
@@ -0,0 +1,404 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complete Responsive Hospital Website Design Tutorial</title>
+
+    <!-- aos css file cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+    <!-- magnific popup css cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
+
+    <!-- bootstrap cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap-grid.min.css">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style 1.css">
+
+</head>
+<body>
+    
+
+<!-- header section starts  -->
+
+<header>
+
+<div class="container">
+
+    <a href="#" class="logo"><span>D</span>iagnose<span>Y</span>ourself.</a>
+
+    <nav class="nav">
+        <ul>
+        <li><a href="http://127.0.0.1:8000/login">Sign in</a></li>
+            <li><a href="#home">home</a></li>
+            <li><a href="#about">about</a></li>
+            <li><a href="#facility">facility</a></li>
+            <li><a href="#review">review</a></li>
+            
+            <li><a href="#post">post</a></li>
+            <li><a href="http://127.0.0.1:8000/contact">Login</a></li>
+            <li><a href="https://web-chat.global.assistant.watson.cloud.ibm.com/preview.html?region=eu-gb&integrationID=0d30222e-66fd-4d91-ad77-26c5bb697e7c&serviceInstanceID=228ef101-4247-4855-bfd9-a67418ca5886">Chatbot</a></li>
+        </ul>
+    </nav>
+
+    <div class="fas fa-bars"></div>
+
+</div>
+
+</header>
+
+<!-- header section ends  -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+<div class="container">
+
+    <div class="row min-vh-100 align-items-center text-center text-md-left">
+
+        <div class="col-md-6 pr-md-5" data-aos="zoom-in">
+            <img src="home-img.svg" width="100%"  alt="">
+        </div>
+
+        <div class="col-md-6 pl-md-5 content" data-aos="fade-left">
+            <h1><span>stay</span> safe, <span>stay</span> healthy.</h1>
+            <h3>caring for you.</h3>
+            <a href="learn more.html"><button class="button">learn more</button></a>
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+
+<!-- home section ends -->
+
+<!-- about section start  -->
+
+<section class="about" id="about">
+
+<div class="container">
+
+    <div class="row min-vh-100 align-items-center">
+
+        <div class="col-md-6 content" data-aos="fade-right">
+
+            <div class="box">
+                <h3> <i class="fas fa-ambulance"></i><a href="https://www.justdial.com/Kanpur/24-Hours-Ambulance-Services/nct-10000352"> Ambulance services </h3></a>
+                <p>If you want amubalance service then press button.</p>
+            </div>
+
+            <div class="box">
+                <h3> <i class="fas fa-procedures"></i><a href="https://www.tatahealth.com/online-doctor-consultation/general-physician?utm_source=google&utm_medium=search_ads&utm_campaign=Online_Doctor_High_Intent&gclid=CjwKCAiAl-6PBhBCEiwAc2GOVIVKMYqHgwgvNxic-tDoA31FirJPwCGusZX97s56k7BK9MXAo3YHlRoCpyEQAvD_BwE"> emergency rooms </h3></a>
+                <p>If you want emergency rooms then press button.</p>
+            </div>
+
+            <div class="box">
+                <h3> <i class="fas fa-stethoscope"></i><a href="https://www.netmeds.com/netmeds-health-pack?source_attribution=ADW-CPC-DiagnosticPack&utm_source=ADW-CPC-DiagnosticPack&utm_medium=CPC&utm_campaign=ADW-CPC-DiagnosticPack&gclid=CjwKCAiAl-6PBhBCEiwAc2GOVOBPh0YBI3f9H_13qlYXUNPUd1qwNm3-vdgXN4BEkKw906VI4fPlzhoCRvoQAvD_BwE"> free check-ups </h3></a>
+                <p>If you want any checkup then press button.</p>
+            </div>
+
+        </div>
+
+        <div class="col-md-6 d-none d-md-block" data-aos="fade-left">
+            <img src="about.png" width="100%" alt="">
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- facility section starts  -->
+
+<section class="facility" id="facility">
+
+<div class="container">
+
+<h1 class="heading"><span>'</span> Our facilities <span>'</span></h1>
+
+<div class="box-container">
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 10.jpg" title="Home remidies">
+            <img src="image 10.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 2.jpg" title="Evpert doctors">
+            <img src="image 2.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 3.jpg" title="Medicines delivery">
+            <img src="image 3.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 4.jpg" title="Group chat">
+            <img src="image 4.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 5.jpg" title="expert doctors">
+            <img src="image 5.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 6.jpg" title="Our team">
+            <img src="image 6.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 7.jpg" title="Communication">
+            <img src="image 7.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 8.jpg" title="">
+            <img src="image 8.jpg" alt="Online medicines">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="image 9.jpg" title="Embulance">
+            <img src="image 9.jpg" alt="">
+        </a>
+    </div>
+
+</div>
+
+</div>
+
+</section>
+
+<!-- facility section ends -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+
+<div class="container">
+
+<h1 class="heading"><span>'</span> people's review <span>'</span></h1>
+
+<div class="box-container">
+
+    <div class="box" data-aos="fade-right">
+        <p>This website is too good its help me alot.</p>
+        <h3>Aditi singh</h3>
+        <span>jan 5, 2021</span>
+        <img src="4.jpg" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-up">
+        <p> Great medical office, wonderful and warm experience from start to finish.</p>
+        <h3>Nishant kaushal</h3>
+        <span>jan 7, 2021</span>
+        <img src="6.jpg" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-left">
+        <p> I immediately felt at ease – and felt confident in my receiving expert medical care. </p>
+        <h3>Soumya  singh</h3>
+        <span>jan 10, 2021</span>
+        <img src="5.jpg" alt="">
+    </div>
+
+</div>
+
+</div>
+
+</section>
+
+<!-- review section ends  -->
+
+<!-- counter section starts  -->
+
+<section class="counter">
+
+    <div class="container">
+
+        <div class="box-container">
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-hospital"></i>
+                <span>120+</span>
+                <h3>Hospitals</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-users"></i>
+                <span>100+</span>
+                <h3>staff</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-smile"></i>
+                <span>1200+</span>
+                <h3>happy patients</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-procedures"></i>
+                <span>130+</span>
+                <h3>bed facility</h3>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- counter section ends -->
+
+<!-- contact section starts  -->
+
+x
+<section class="post" id="post">
+
+    <div class="container min-vh-100">
+
+        <h1 class="heading"><span>'</span> out posts <span>'</span></h1>
+
+        <div class="box-container">
+
+            <div class="box" data-aos="fade-right">
+                <img src="1.jpg" alt="">
+                <div class="content">
+                    <span>jan 5, 2021</span>
+                    <a href="#"><h3>post title goes here</h3></a>
+                    <p>The services that I receive from (DN) is excellent.</p>
+                    <a href="#"><button class="button">learn more</button></a>
+                </div>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <img src="2.jpg" alt="">
+                <div class="content">
+                    <span>jan 5, 2021</span>
+                    <a href="#"><h3>post title goes here</h3></a>
+                    <p>Great medical office, wonderful and warm experience from start to finish</p>
+                    <a href="#"><button class="button">learn more</button></a>
+                </div>
+            </div>
+
+            <div class="box" data-aos="fade-left">
+                <img src="3.jpg" alt="">
+                <div class="content">
+                    <span>jan 5, 2021</span>
+                    <a href="#"><h3>post title goes here</h3></a>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex porro libero at veniam molestias facere quo necessitatibus ipsum ad?</p>
+                    <a href="#"><button class="button">learn more</button></a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- post section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4" data-aos="fade-right">
+                <a href="#" class="logo"><span>H</span>ealth<span>C</span>are.</a>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur nemo porro quasi minima consequuntur dolorum, quas amet in autem id?</p>
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-up">
+                <h3>links</h3>
+                <a href="#">home</a>
+                <a href="#">about</a>
+                <a href="#">facility</a>
+                <a href="#">review</a>
+                <a href="#">contact</a>
+                <a href="#">post</a>
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-left">
+                <h3>share</h3>
+                <a href="#">facebook</a>
+                <a href="#">twitter</a>
+                <a href="#">instagram</a>
+                <a href="#">github</a>
+            </div>
+
+        </div>
+
+    </div>
+
+    <h1 class="credit text-center mx-auto">Thanku <span>for your visit</span> | all rights reserved.</h1>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- jquery cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- magnific popup js link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+
+<!-- aos js file cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+<!-- custom js link  -->
+<script src="js/main 1.js"></script>
+
+
+<script>
+
+AOS.init({
+    duration:1000,
+    delay:400
+});
+
+</script>
+
+</body>
+</html>
